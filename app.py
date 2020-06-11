5111ab83f531ab3ab0a6e3f15dc59e49b2fce9de
yokosan0803 <yoko.san.bb@gmail.com>
2020-06-11 19:08:29 +0900

    addを作成

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+# sqlite3を使えるようにする
+import sqlite3
+
+from flask import Flask,render_template,request
 # flaskのflask,render_templateを使用します宣言
 app= Flask(__name__)
 
@@ -18,15 +21,63 @@ def test2():
     name = "flask"
     return render_template("test2.html",name = name)
 
+# 404ページを表示
+@app.errorhandler(404)
+def notfound(code):
+    return "ここは404ページです"
+
+
+
+# 2日目データベースの接続
+@app.route("/dbtest")
+def dbtest():
+    # データベースに接続
+    conn = sqlite3.connect('flask.db')
+    
+    # どこのデータを抜くかカーソルを充てる。カーソル→矢印
+    c = conn.cursor()
+    
+    # execute:実行する
+    c.execute("select name, adress from users where id = 2")
+    
+    # fetchone:フェッチ：実際に取得する
+    user_info = c.fetchone()
+    
+    # データベース接続終了
+    c.close()
+    
+    # user_infoの中身を確認
+    print(user_info)
+    return render_template("dbtest.html",user_info = user_info)
+
+
+
+# データベースを追加
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+# データを追加するボタンの処理
+@app.route("/add",methods=["POST"])
+def add_post():
+    # add.htmlからformのname="task"を取得
+    task = request.form.get("task")
+    # データベースに接続
+    conn = sqlite3.connect("flask.db")
+    c = conn.cursor()
+    # (task,)のカンマは忘れずに！タプル型なので！
+    # ?に(task,)が入るよ
+    # insert into はデータを追加
+    c.execute("insert into task values(null ,?)", (task,))
+    conn.commit()
+    c.close()
+    return "データを追加できました！"
 
 
 
 
 
-
-
-
-
+# 一番下に書くよ
 if __name__ == "__main__":
     # サーバーを起動するよ
     app.run(debug=True)
